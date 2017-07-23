f4c81d6a57d320a8d9977d233e1c58fdf29af93c
Naomath <math1972naonao@gmail.com>
2017-07-23 11:27:13 +0900

    Initial commit

---
+++ app/src/main/java/com/lifeistech/naoto/sugarormtestaugast/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lifeistech.naoto.sugarormtestaugast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
